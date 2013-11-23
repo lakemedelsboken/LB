96fe54645fbf5e181ee7cd5fa10c2301c2642e6b
Staffan Living <staffan.living@gmail.com>
2013-11-23 12:46:09 +0100

    Switched to 3 saved releases on servers

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :branch, 'master'
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :keep_releases, 5
+set :keep_releases, 3
 #set :pass, fetch(:pass, '')
 
 namespace :deploy do
