a88cdbf46fba3f2ae1d94fa2ce7f7a7705da6773
Staffan Living <staffan.living@gmail.com>
2013-11-23 12:33:03 +0100

    Fixed blog issues related to pm2 0.6.7 and process.cwd()

--- servers/blog/app.js
+++ servers/blog/app.js
@@ -11,9 +11,13 @@ var networkPort = settings.internalServerPorts.blog;
 
 app.listen(networkPort);
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('jojo basepath', '/blog/');
 app.set('jojo articles', __dirname + '/articles/');
+app.set('jojo index view', __dirname + '/views/pages/index');
+app.set('jojo article view', __dirname + '/views/pages/article');
+app.set('jojo rss view', __dirname + '/views/xml');
 //app.use("/blog/", express.static(process.cwd() + '/public'));
 
 console.log('Blog server is listening to http://127.0.0.1:' + networkPort);
