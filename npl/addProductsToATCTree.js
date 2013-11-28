12aace097aa7cc36aea8c0d6de01e9ad72087d31
Staffan Living <staffan.living@gmail.com>
2013-11-28 12:04:17 +0100

    Some error checking when adding products to ATC-tree

--- npl/addProductsToATCTree.js
+++ npl/addProductsToATCTree.js
@@ -42,81 +42,87 @@ for (var i = 0; i < fileNames.length; i++) {
 				}
 				
 				var parent = getItemById(product.atcCode);
-				parent.hasChildren = true;
+				if (parent === undefined) {
+					console.log("There was not atc for: " + product.atcCode);
+				} else {
+
+					parent.hasChildren = true;
 				
-				var item = getItemById(product.id);
-				if (item === undefined) {
-					item = {
-						id: product.id,
-						parentId: product.atcCode,
-						title: product.name + ", " + description + ", " + product.brand,
-						type: "product",
-						titlePath: parent.titlePath,
-						idPath: parent.idPath
-					};
-					add = true;
+					var item = getItemById(product.id);
+					if (item === undefined) {
+						item = {
+							id: product.id,
+							parentId: product.atcCode,
+							title: product.name + ", " + description + ", " + product.brand,
+							type: "product",
+							titlePath: parent.titlePath,
+							idPath: parent.idPath
+						};
+						add = true;
 					
-				}
-
-				/*
-				"id": "20091203000076",
-				"parentId": "D08",
-				"title": "Calendula Weleda, Kutan lösning , Weleda",
-				"type": "product",
-				"titlePath": "Hudpreparat / Antiseptika och sårmedel",
-				"idPath": "D / D08",
-				"indications": "",
-				"substance": "",
-				"parallelimport": "",
-				"noinfo": true
-			
-				*/
-
-				if (product.noinfo === undefined && product.images !== undefined && product.images.length > 0) {
-					var images = product.images;
-					var imagesToSave = [];
-					for (var j=0; j < images.length; j++) {
-						var imageFilePath = productsPath + "images/" + images[j].checksum + ".jpg";
-						if (fs.existsSync(imageFilePath)) {
-							var imageWebPath = "/products/images/" + images[j].checksum + ".jpg";
-							imagesToSave.push(imageWebPath);
-						}
 					}
-					//Add to atc tree
-					item.images = imagesToSave;
-					item.noinfo = false;
-				} 
 
-				if (product.noinfo === undefined && product.sections !== undefined && product.sections["Indikationer"] !== undefined) {
-					var indications = $(product.sections["Indikationer"]).text().replace(/\n/g, " ");
-					//Add to atc tree
-					item.indications = indications;
-				} else {
-					item.indications = "";
-				} 
+					/*
+					"id": "20091203000076",
+					"parentId": "D08",
+					"title": "Calendula Weleda, Kutan lösning , Weleda",
+					"type": "product",
+					"titlePath": "Hudpreparat / Antiseptika och sårmedel",
+					"idPath": "D / D08",
+					"indications": "",
+					"substance": "",
+					"parallelimport": "",
+					"noinfo": true
+			
+					*/
 
-				if (product.noinfo === undefined && product.substance !== undefined) {
-					//Add to atc tree
-					item.substance = product.substance;
-				} else {
-					item.substance = "";
-				} 
+					if (product.noinfo === undefined && product.images !== undefined && product.images.length > 0) {
+						var images = product.images;
+						var imagesToSave = [];
+						for (var j=0; j < images.length; j++) {
+							var imageFilePath = productsPath + "images/" + images[j].checksum + ".jpg";
+							if (fs.existsSync(imageFilePath)) {
+								var imageWebPath = "/products/images/" + images[j].checksum + ".jpg";
+								imagesToSave.push(imageWebPath);
+							}
+						}
+						//Add to atc tree
+						item.images = imagesToSave;
+						item.noinfo = false;
+					} 
 
-				if (product.noinfo === undefined && product.parallelimport !== undefined && product.parallelimport !== "") {
-					//Add to atc tree
-					item.parallelimport = product.parallelimport;
-				} else {
-					item.parallelimport = "";
-				} 
+					if (product.noinfo === undefined && product.sections !== undefined && product.sections["Indikationer"] !== undefined) {
+						var indications = $(product.sections["Indikationer"]).text().replace(/\n/g, " ");
+						//Add to atc tree
+						item.indications = indications;
+					} else {
+						item.indications = "";
+					} 
 
-				if (product.noinfo !== undefined) {
-					item.noinfo = true;
-				} else {
-					item.noinfo = false;
-				}
+					if (product.noinfo === undefined && product.substance !== undefined) {
+						//Add to atc tree
+						item.substance = product.substance;
+					} else {
+						item.substance = "";
+					} 
+
+					if (product.noinfo === undefined && product.parallelimport !== undefined && product.parallelimport !== "") {
+						//Add to atc tree
+						item.parallelimport = product.parallelimport;
+					} else {
+						item.parallelimport = "";
+					} 
+
+					if (product.noinfo !== undefined) {
+						item.noinfo = true;
+					} else {
+						item.noinfo = false;
+					}
 				
-				if (add) {
-					atcTree.push(item);
+					if (add) {
+						atcTree.push(item);
+					}
+					
 				}
 			}
 			
