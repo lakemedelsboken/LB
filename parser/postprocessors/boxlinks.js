d3f25a01d3f1ea30d6592104a7a43fabddb0d377
Staffan Living <staffan.living@gmail.com>
2013-12-10 22:24:15 +0100

    Updated white space bug

--- parser/postprocessors/boxlinks.js
+++ parser/postprocessors/boxlinks.js
@@ -23,6 +23,8 @@ module.exports = injector = {
 			text = "<body>" + text + "</body>";
 		}
 
+		//console.log(text);
+
 		$ = cheerio.load(text);
 		
 		var body = $("body").first();
@@ -78,31 +80,38 @@ module.exports = injector = {
 		}
 		
 		//Add a space for regex operations
-		text = " " + text;
+//		text = " " + text;
 
-		text = text.replace(/[ (]Figur\s[0-9\-]+/g, function(match) {
-			var firstChar = match.substr(0, 1);
-			match = match.substr(1);
-			var numbers = extractNumbers(match.split(" ")[1]);
-			return " <a class=\"btn btn-small figureLink\" href=\"#figure_" + numbers[0] + "\" data-numbers=\"" + numbers.join(",") + "\">" + match + "</a>";
+//		text = text.replace(/[ (]Figur\s[0-9\-]+/g, function(match) {
+		text = text.replace(/Figur\s[0-9]+/g, function(match) {
+			//var firstChar = match.substr(0, 1);
+			//match = match.substr(1);
+			//console.log("Extract number from: \"" + match.split(" ") + "\"");
+			var numbers = extractNumbers(match.split(/\s/)[1]);
+			return "<a class=\"btn btn-small figureLink\" href=\"#figure_" + numbers[0] + "\" data-numbers=\"" + numbers.join(",") + "\">" + match + "</a>";
 		});
 
-		text = text.replace(/[ (]Tabell\s[0-9\-]+/g, function(match) {
-			var firstChar = match.substr(0, 1);
-			match = match.substr(1);
-			var numbers = extractNumbers(match.split(" ")[1]);
-			return " <a class=\"btn btn-small tableLink\" href=\"#table_" + numbers[0] + "\" data-numbers=\"" + numbers.join(",") + "\">" + match + "</a>";
+//		text = text.replace(/[ (]Tabell\s[0-9\-]+/g, function(match) {
+		text = text.replace(/Tabell\s[0-9]+/g, function(match) {
+			//var firstChar = match.substr(0, 1);
+			//match = match.substr(1);
+			//console.log("Extract number from: \"" + match.split(" ") + "\"");
+			var numbers = extractNumbers(match.split(/\s/)[1]);
+			return "<a class=\"btn btn-small tableLink\" href=\"#table_" + numbers[0] + "\" data-numbers=\"" + numbers.join(",") + "\">" + match + "</a>";
 		});
 
-		text = text.replace(/[ (]Faktaruta\s[0-9\-]+/g, function(match) {
-			var firstChar = match.substr(0, 1);
-			match = match.substr(1);
-			var numbers = extractNumbers(match.split(" ")[1]);
-			return firstChar + "<a class=\"btn btn-small factsLink\" href=\"#facts_" + numbers[0] + "\" data-numbers=\"" + numbers.join(",") + "\">" + match + "</a>";
+//		text = text.replace(/[ (]Faktaruta\s[0-9\-]+/g, function(match) {
+		text = text.replace(/Faktaruta\s[0-9]+/g, function(match) {
+			//var firstChar = match.substr(0, 1);
+			//match = match.substr(1);
+			//console.log("Extract number from: \"" + match.split(/\s/) + "\"");
+			var numbers = extractNumbers(match.split(/\s/)[1]);
+			//return firstChar + "<a class=\"btn btn-small factsLink\" href=\"#facts_" + numbers[0] + "\" data-numbers=\"" + numbers.join(",") + "\">" + match + "</a>";
+			return "<a class=\"btn btn-small factsLink\" href=\"#facts_" + numbers[0] + "\" data-numbers=\"" + numbers.join(",") + "\">" + match + "</a>";
 		});
 
 		//Remove the added space
-		text = text.substr(1);
+		//text = text.substr(1);
 		return text;
 		
 	},
