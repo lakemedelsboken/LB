091e378a35b61b4331c72d19b47a7347ce91020e
Staffan Living <staffan.living@gmail.com>
2014-10-06 21:36:36 +0200

    Added version id to scripts in blog output

--- servers/blog/app.js
+++ servers/blog/app.js
@@ -18,6 +18,8 @@ app.set('jojo articles', __dirname + '/articles/');
 app.set('jojo index view', __dirname + '/views/pages/index');
 app.set('jojo article view', __dirname + '/views/pages/article');
 app.set('jojo rss view', __dirname + '/views/xml');
+
+jojo.config.version = settings.version;
 //app.use("/blog/", express.static(process.cwd() + '/public'));
 
 console.log('Blog server is listening to http://127.0.0.1:' + networkPort);
