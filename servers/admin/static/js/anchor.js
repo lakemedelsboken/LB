3198b4e6031109657cc9bf463d50392eb7cf99ca
Staffan Living <staffan.living@gmail.com>
2014-11-04 23:42:00 +0100

    Fixed indentation

--- servers/admin/static/js/anchor.js
+++ servers/admin/static/js/anchor.js
@@ -4,25 +4,36 @@
 * Copyright (c) 2014 Bryan Braun; Licensed MIT
 */
 function addAnchors(selector) {
-	// Sensible default selector, if none is provided.
-	selector = selector || '.chapterContent h1, .chapterContent h2, .chapterContent h3, .chapterContent h4, .chapterContent h5, .chapterContent h6';
+
+	if (!ltIE9) {
+		// Sensible default selector, if none is provided.
+		selector = selector || '.chapterContent h1, .chapterContent h2, .chapterContent h3, .chapterContent h4, .chapterContent h5, .chapterContent h6';
   
-	// Select any elements that match the provided selector.
-	var elements = $(selector);
+		// Select any elements that match the provided selector.
+		var elements = $(selector);
 
-	// Loop through the selected elements.
-  	elements.each(function() {
+		// Loop through the selected elements.
+	  	elements.each(function() {
 		
-		var element = $(this);
-		var elementID = undefined;
+			var element = $(this);
+			var elementID = undefined;
 
-		if (!!element.attr('id') && element.attr('id') !== "") {
-			elementID = element.attr('id');
-		}
+			if (!!element.attr('id') && element.attr('id') !== "") {
+				elementID = element.attr('id');
+			}
 		
-		if (elementID !== undefined) {
-			var anchor = '<a class="anchor-link" href="?id=' + elementID + '#' + elementID + '" title="Direktlänk till rubrik"><span class="icon-link"></span></a>';
-			element.html(element.html() + anchor);
-		}
-	});
+			if (elementID !== undefined) {
+				var pageUrl = "";
+				if (window.location.hash !== "" && window.location.hash.indexOf(".html") > -1) {
+					pageUrl = window.location.hash.split(".html");
+					pageUrl = pageUrl[0];
+					pageUrl += ".html";
+					pageUrl = pageUrl.replace("#", "");
+				}
+				var anchor = '<a class="anchor-link" href="' + pageUrl + '?id=' + elementID + '#' + elementID + '" title="Direktlänk till rubrik"><span class="icon-link"></span></a>';
+				element.html(element.html() + anchor);
+			}
+		});
+	}
+
 }
