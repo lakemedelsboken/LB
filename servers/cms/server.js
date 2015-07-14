35d943289cb28c3005e4e0f2a2a23b9b2d82582d
Staffan Living <staffan.living@gmail.com>
2015-07-15 01:30:17 +0200

    Minor tweak

--- servers/cms/server.js
+++ servers/cms/server.js
@@ -8,5 +8,5 @@ var app = require('./app');
 app.set('port', networkPort);
 
 var server = app.listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + server.address().port);
+	console.log('CMS server listening on port ' + server.address().port);
 });
