481f7ff8c4f7e39f223cf3471bd1902effdff5bd
Staffan Living <staffan.living@gmail.com>
2015-07-15 01:44:02 +0200

    Switched to uglifyjs2

--- build/javascripts.sh
+++ build/javascripts.sh
@@ -6,15 +6,18 @@
 
 cat ../servers/cms/output/static/js/uncompressed/jquery.js ../servers/cms/output/static/js/uncompressed/jquery.history.js ../servers/cms/output/static/js/uncompressed/twitter-dev/bootstrap-tooltip.js ../servers/cms/output/static/js/uncompressed/twitter-dev/bootstrap-popover.js ../servers/cms/output/static/js/uncompressed/clickover.js ../servers/cms/output/static/js/uncompressed/jquery.cookie.js ../servers/cms/output/static/js/uncompressed/jquery.hotkeys.js ../servers/cms/output/static/js/uncompressed/jquery-ui-1.10.2.custom.js ../servers/cms/output/static/js/uncompressed/jquery-scrollto.js ../servers/cms/output/static/js/uncompressed/jquery.animate-enhanced.js ../servers/cms/output/static/js/uncompressed/matchmedia.js ../servers/cms/output/static/js/uncompressed/respond.js ../servers/cms/output/static/js/uncompressed/picturefill.js ../servers/cms/output/static/js/uncompressed/anchor.js ../servers/cms/output/static/js/uncompressed/lb.js > newscripts.js
 
+#../servers/cms/output/static/js/uncompressed/fingerprint.js
+
 #uglifyjs scripts.js -nc > ../servers/admin/static/js/scripts.min.js
 
 #Old
 #uglifyjs scripts.js -nc > ../servers/site/static/js/scripts.min.js
 
-uglifyjs newscripts.js -nc > ../servers/cms/output/static/js/scripts.min.js
+#uglifyjs newscripts.js -m -c > ../servers/cms/output/static/js/scripts.min.js
+uglifyjs ../servers/cms/output/static/js/uncompressed/jquery.js ../servers/cms/output/static/js/uncompressed/jquery.history.js ../servers/cms/output/static/js/uncompressed/twitter-dev/bootstrap-tooltip.js ../servers/cms/output/static/js/uncompressed/twitter-dev/bootstrap-popover.js ../servers/cms/output/static/js/uncompressed/clickover.js ../servers/cms/output/static/js/uncompressed/jquery.cookie.js ../servers/cms/output/static/js/uncompressed/jquery.hotkeys.js ../servers/cms/output/static/js/uncompressed/jquery-ui-1.10.2.custom.js ../servers/cms/output/static/js/uncompressed/jquery-scrollto.js ../servers/cms/output/static/js/uncompressed/jquery.animate-enhanced.js ../servers/cms/output/static/js/uncompressed/matchmedia.js ../servers/cms/output/static/js/uncompressed/respond.js ../servers/cms/output/static/js/uncompressed/picturefill.js ../servers/cms/output/static/js/uncompressed/anchor.js ../servers/cms/output/static/js/uncompressed/lb.js -m -c > ../servers/cms/output/static/js/scripts.min.js
 #cp newscripts.js ../servers/cms/output/static/js/scripts.min.js
 
-uglifyjs ../servers/api/scripts/lb.injectgenericas.js -nc > ../servers/api/scripts/lb.injectgenericas.min.js
+uglifyjs ../servers/api/scripts/lb.injectgenericas.js -m -c > ../servers/api/scripts/lb.injectgenericas.min.js
 
 #cp scripts.js ../servers/admin/static/js/scripts.min.js
 #cp scripts.js ../servers/site/static/js/scripts.min.js
