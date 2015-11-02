c3b8fdaa1b37265f893dc1f38111d4d67ba63013
Staffan Living <staffan.living@gmail.com>
2015-11-03 00:31:42 +0100

    Auto commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-set :keep_releases, 3
+set :keep_releases, 2
 
 namespace :deploy do
 
