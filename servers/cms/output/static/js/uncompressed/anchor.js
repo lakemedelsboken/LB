c201754e08505260851ddf67f675248d11dd457e
Oskar Svärd <oskar.svard@mac.com>
2016-10-24 10:55:55 +0200

    Style changes

--- servers/cms/output/static/js/uncompressed/anchor.js
+++ servers/cms/output/static/js/uncompressed/anchor.js
@@ -6,28 +6,29 @@
 function addAnchors(selector) {
 
 	var ltIE9 = true;
-	
+
 	if (document.addEventListener){
 		ltIE9 = false;
 	}
+	
 
 	if (!ltIE9) {
 		// Sensible default selector, if none is provided.
 		selector = selector || '.chapterContent h1, .chapterContent h2, .chapterContent h3, .chapterContent h4, .chapterContent h5, .chapterContent h6';
-  
+
 		// Select any elements that match the provided selector.
 		var elements = $(selector);
 
 		// Loop through the selected elements.
 	  	elements.each(function() {
-		
+
 			var element = $(this);
 			var elementID = undefined;
 
 			if (!!element.attr('id') && element.attr('id') !== "") {
 				elementID = element.attr('id');
 			}
-		
+
 			if (elementID !== undefined) {
 				var pageUrl = "";
 				if (window.location.hash !== "" && window.location.hash.indexOf(".html") > -1) {
