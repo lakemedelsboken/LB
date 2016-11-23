b6e87448388a58ef8892732a1262ed5d1f58ae9b
Oskar Svärd <oskar.svard@mac.com>
2016-11-23 12:55:22 +0100

    New logic to fetch the nil file.

---
+++ npl/fetchNplFile.js
@@ -0,0 +1,27 @@
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+
+
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(path.join(__dirname, dest));
+  var request = https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
+
+var today = new Date();
+var d = today.toISOString().substring(0, 10);
+
+download('https://npl4.mpa.se/MpaProductExport/'+d+'_Total_NPL.zip', '/database/npl/npl.zip', function (err) {
+	if (err) {
+		console.error('Download of npl failed');
+	}
+	console.log('Download of npl finished');
+});
