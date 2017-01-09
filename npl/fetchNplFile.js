efd3ee7547ebab76f0d93ed71e086887648089df
Oskar Svärd <oskar.svard@mac.com>
2017-01-09 09:14:02 +0100

    Changed npl4.mpa.se to npl.mpa.se. Added a flag to accept unleafy certificates. Issue to npl.mpa.se sent.

--- npl/fetchNplFile.js
+++ npl/fetchNplFile.js
@@ -5,6 +5,7 @@ var path = require('path');
 
 var download = function(url, dest, cb) {
   var file = fs.createWriteStream(path.join(__dirname, dest));
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
   var request = https.get(url, function(response) {
     response.pipe(file);
     file.on('finish', function() {
@@ -19,7 +20,7 @@ var download = function(url, dest, cb) {
 var today = new Date();
 var d = today.toISOString().substring(0, 10);
 
-download('https://npl4.mpa.se/MpaProductExport/'+d+'_Total_NPL.zip', '/database/npl/npl.zip', function (err) {
+download('https://npl.mpa.se/MpaProductExport/'+d+'_Total_NPL.zip', '/database/npl/npl.zip', function (err) {
 	if (err) {
 		console.error('Download of npl failed');
 	}
