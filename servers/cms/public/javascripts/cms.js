63d15ce1e48376e1bdbccb36284f22a91054b807
Rami Sulebi <rami.sulebi@consid.se>
2017-10-13 16:23:01 +0200

    delete a new pdf button in Cms and rebuild all javascript files

--- servers/cms/public/javascripts/cms.js
+++ servers/cms/public/javascripts/cms.js
@@ -741,7 +741,7 @@ $(document).ready(function() {
 			$("#draftFrame").prop("src", url);
 			$("#draftLink").prop("href", url);
 			$("#draftPdfLink").prop("href", "/pdf/download?style=old&url=" + encodeURIComponent(url));
-			$("#draftNewPdfLink").prop("href", "/pdf/download?style=single-column&url=" + encodeURIComponent(url));
+			//$("#draftNewPdfLink").prop("href", "/pdf/download?style=single-column&url=" + encodeURIComponent(url));
 			$("#draftWordLink").prop("href", "/cms/content/docx/download?url=" + encodeURIComponent(url));
 
 		});
@@ -763,7 +763,7 @@ $(document).ready(function() {
 			$("#publishedFrame").prop("src", url);
 			$("#publishedLink").prop("href", url);
 			$("#publishedPdfLink").prop("href", "/pdf/download?style=old&url=" + encodeURIComponent(url));
-			$("#publishedNewPdfLink").prop("href", "/pdf/download?style=single-column&url=" + encodeURIComponent(url));
+			//$("#publishedNewPdfLink").prop("href", "/pdf/download?style=single-column&url=" + encodeURIComponent(url));
 			$("#publishedWordLink").prop("href", "/cms/content/docx/download?url=" + encodeURIComponent(url));
 
 		});
